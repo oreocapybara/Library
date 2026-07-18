c9500816ec756df749f0eb21a4f0a5ba838f772e
oreocapybara <71880256+oreocapybara@users.noreply.github.com>
2026-07-18 17:11:31 +0800

    refactor(book): use class functions to declare book object
    - Created IIFE (Immediately Invoked Function Execution) to have closure
      and data privacy

--- script.js
+++ script.js
@@ -3,51 +3,50 @@ const addButton = document.querySelector(".add-btn");
 const dialog = document.querySelector("dialog");
 const form = document.querySelector("form");
 
-// Event listener for the add button
-addButton.addEventListener("click", () => {
-	dialog.showModal();
-	console.log('addButton clicked');
-});
+// Class
+class Book {
+	// Class constructor
+	constructor(title, author, pages, read = false) {
+		this.id = crypto.randomUUID();
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+	}
+}
 
-form.addEventListener("submit", (e) => {
-	e.preventDefault(); // Prevent empty form submission
+(function () {
+	const myLibrary = []; //Array to store book instances
+	
+	// Event listener for the add button
+	addButton.addEventListener("click", () => {
+		dialog.showModal();
+	});
 
-	const title = form.elements["title"].value;
-	const author = form.elements["author"].value;
-	const pages = form.elements["pages"].value;
-	const read = form.elements["read"].checked;
+	form.addEventListener("submit", (e) => {
+		e.preventDefault(); // Prevent empty form submission
 
-	addBookToLibrary(title, author, pages, read);
-	dialog.close();
-	form.reset();
-});
+		const title = form.elements["title"].value;
+		const author = form.elements["author"].value;
+		const pages = form.elements["pages"].value;
+		const read = form.elements["read"].checked;
 
-const myLibrary = []; //Array to store book instances
+		addBookToLibrary(title, author, pages, read);
+		dialog.close();
+		form.reset();
+	});
 
-// Object Constructor
-function Book(title, author, pages, read = false) {
-	if (!new.target) {
-		throw Error("The new operator must be used to call the constructor");
+
+	// Add New Book Instance to Array
+	function addBookToLibrary(title, author, pages, read) {
+		let newBook = new Book(title, author, pages, read);
+		myLibrary.push(newBook);
+
+		displaySingleBook(newBook);
 	}
 
-	this.id = crypto.randomUUID();
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
-}
-
-// Add New Book Instance to Array
-function addBookToLibrary(title, author, pages, read) {
-	let newBook = new Book(title, author, pages, read);
-	myLibrary.push(newBook);
-
-	displaySingleBook(newBook);
-}
-
-// Initial function to display all existing books
-function displayALLBooks() {
-	myLibrary.forEach((book) => {
+	// function to display single book instance
+	function displaySingleBook(book) {
 		const bookContainer = document.createElement("div");
 		bookContainer.dataset.id = book.id;
 
@@ -66,29 +65,6 @@ function displayALLBooks() {
 		bookContainer.append(headerTitle, author, pages, status);
 
 		bookLibrary.append(bookContainer);
-	});
-}
+	}
+})();
 
-// function to display single book instance
-function displaySingleBook(book) {
-	const bookContainer = document.createElement("div");
-	bookContainer.dataset.id = book.id;
-
-	const headerTitle = document.createElement("h1");
-	headerTitle.textContent = book.title;
-
-	const author = document.createElement("h3");
-	author.textContent = book.author;
-
-	const pages = document.createElement("p");
-	pages.textContent = `${book.pages} pages`;
-
-	const status = document.createElement("p");
-	status.textContent = book.read ? "Read" : "Not read yet";
-
-	bookContainer.append(headerTitle, author, pages, status);
-
-	bookLibrary.append(bookContainer);
-}
-
-displayALLBooks();
